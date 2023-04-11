a220f7193e7e463f60862f9317941ff795a5ac1e
sukeshkoramutla <sukeshreddy831@gmail.comn>
2023-04-11 23:03:31 +0530

    done all tasks

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String args[]){
         RWOnly rw = new RWOnly("Sukesh");
         rw.setName("Sukesh Reddy");
-        System.out.println(rw.getName());
+        String name = rw.getName();
+        System.out.println(name);
     }
 }
