c28668a7c7a27addc8d87f966ffc444da7934384
sukeshkoramutla <sukeshreddy831@gmail.comn>
2023-04-12 15:14:24 +0530

    final commit

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,11 +1,8 @@
 package com.driver;
 
 public class RWOnly {
-    String name;
-    RWOnly(String name){
-        this.name = name;
-        System.out.println(name);
-    }
+    private String name;
+    RWOnly(){ }
 
     public String getter() {
         return name;
